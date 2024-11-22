aec0833901cb8d51cb0db94e129edf3ed0b30b58
Zihag <nghia22102002@gmail.com>
2024-11-23 01:30:24 +0700

    scrollable-bottom-sheet

--- lib/app_colors/app_colors.dart
+++ lib/app_colors/app_colors.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const Color cyan = Color(0xFF71C9CE);
+  static const Color blueYesColor = Color(0xFF30E3CA);
+  static const Color redNoColor = Color(0xFFFF6264);
 }
